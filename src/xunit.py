88e0040769f65d94c2b09533940bad2133ab73c3
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2023-02-16 21:45:39 +0900

    第19章

--- src/xunit.py
+++ src/xunit.py
@@ -2,27 +2,38 @@ class TestCase:
     def __init__(self, name: str):
         self.name = name
 
+    def setUp(self) -> None:
+        pass
+
     def run(self) -> None:
+        self.setUp()
         # 自身のインスタンスの名前(つまりテストメソッド名)を取得し、それを実行する
         method = getattr(self, self.name)
         method()
 
 
 class WasRun(TestCase):
-    def __init__(self, name: str):
+
+    def setUp(self) -> None:
         self.wasRun: int | None = None
-        super().__init__(name)
+        self.wasSetUp = 1
 
     def testMethod(self) -> None:
         self.wasRun = 1
 
 
 class TestCaseTest(TestCase):
+    def setUp(self) -> None:
+        self.test = WasRun("testMethod")
+
     def testRunning(self) -> None:
-        test = WasRun("testMethod")
-        assert (not test.wasRun)
-        test.run()
-        assert (test.wasRun)
+        self.test.run()
+        assert (self.test.wasRun)
+
+    def testSetUp(self) -> None:
+        self.test.run()
+        assert (self.test.wasSetUp)
 
 
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
